ea9dc922ab9a5eec224d273098a00d73569e23f2
asanchezyali <asanchezyali@gmail.com>
2022-07-13 23:36:35 -0500

    add test to add weight and oriented neighbors

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -35,5 +35,28 @@ def test_add_unweighted_and_unoriented_neighbors():
     assert edge_ab.start == None
     assert edge_ab.end == None
     assert edge_ab.left_vertex == vertex_a
-    assert edge_ab.right_1vertex == vertex_b
+    assert edge_ab.right_vertex == vertex_b
     assert edge_ab.loop == False
+
+def test_add_weighted_and_oriented_neighbors():
+    vertex_a = Vertex('a')
+    vertex_b = Vertex('b')
+    vertex_a.add_neighbor(vertex_b, weight=1.0, orientation='->')
+    edge_ab = vertex_a.edge('a-b')
+    assert vertex_b.label in vertex_a.neighbors.keys()
+    assert vertex_b in vertex_a.neighbors.values()
+    assert vertex_a.label in vertex_b.neighbors.keys()
+    assert vertex_a in vertex_b.neighbors.values()
+    assert 'a-b' in vertex_a.edges.keys()
+    assert 'a-b' in vertex_b.edges.keys()
+    assert vertex_a.loops == dict()
+    assert vertex_b.loops == dict()
+    assert edge_ab.label == 'a-b'
+    assert edge_ab.weight == 1.0
+    assert edge_ab.orientation == '->'
+    assert edge_ab.start == vertex_a
+    assert edge_ab.end == vertex_b
+    assert edge_ab.left_vertex == vertex_a
+    assert edge_ab.right_vertex == vertex_b
+    assert edge_ab.loop == False
+    
