b73c8405109334264653a46b65c0072e236205e9
asanchezyali <asanchezyali@gmail.com>
2022-07-13 22:45:24 -0500

    update test to add unweighted and unoriented neighbors

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -16,21 +16,24 @@ def test_delete_vertex():
     except NameError:
         assert True
 
-def test_add_neighbor_without_weight():
+def test_add_unweighted_and_unoriented_neighbors():
     vertex_a = Vertex('a')
     vertex_b = Vertex('b')
     vertex_a.add_neighbor(vertex_b)
+    edge_ab = vertex_a.edge('a-b')
     assert vertex_b.label in vertex_a.neighbors.keys()
     assert vertex_b in vertex_a.neighbors.values()
     assert vertex_a.label in vertex_b.neighbors.keys()
     assert vertex_a in vertex_b.neighbors.values()
     assert 'a-b' in vertex_a.edges.keys()
     assert 'a-b' in vertex_b.edges.keys()
-
-def test_add_neighbor_with_weight():
-    pass
-
-
-        
-    
-    
+    assert vertex_a.loops == dict()
+    assert vertex_b.loops == dict()
+    assert edge_ab.label == 'a-b'
+    assert edge_ab.weight == None
+    assert edge_ab.orientation == None
+    assert edge_ab.start == None
+    assert edge_ab.end == None
+    assert edge_ab.left_vertex == vertex_a
+    assert edge_ab.right_1vertex == vertex_b
+    assert edge_ab.loop == False
