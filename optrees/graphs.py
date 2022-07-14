8f90d21fae20095d9013bc8d75e7b51e7e757f49
asanchezyali <asanchezyali@gmail.com>
2022-07-13 22:47:16 -0500

    remove comments from graphs

--- optrees/graphs.py
+++ optrees/graphs.py
@@ -6,37 +6,26 @@ class Vertex:
         self.__loops = dict()
 
     def __del__(self):
-        """
-        Delete the vertex.
-        """
         print(f"Vertex {self.label} is deleted.")
 
     @property
     def label(self) -> str:
-        """
-        Returns the label of the vertex.
-        """
         return self.__label
 
     @property
     def neighbors(self) -> dict:
-        """
-        Return the neighbors of the vertex.
-        """
         return self.__neighbors
 
-    def add_neighbor(self, vertex: "Vertex", weight: float = None):
-        """
-        Add a neighbor to the vertex.
-        """
+    def add_neighbor(self, vertex: "Vertex", weight: float = None, orientation: float = None):
         if self is not vertex:
             self.__neighbors[vertex.label] = vertex
             edge = Edge(
                 label=f"{self.label}-{vertex.label}",
-                lvertex=self,
-                rvertex=vertex,
+                left_vertex=self,
+                right_vertex=vertex,
+                weight=weight,
+                orientation=orientation
             )
-            edge.weight = weight
             self.add_edge(edge)
             vertex.add_edge(edge)
             vertex.__neighbors[self.label] = self
@@ -44,9 +33,6 @@ class Vertex:
             raise ValueError("It is the same vertex.")
 
     def delete_neighbor(self, vertex_label: str):
-        """
-        Delete a neighbor from the vertex.
-        """
         try:
             del self.__neighbors[vertex_label]
             for edge in self.__edges.values():
@@ -57,62 +43,35 @@ class Vertex:
             raise KeyError(f"Vertex {vertex_label} does not exist.")
 
     def neighbor(self, label: str) -> "Vertex":
-        """
-        Return the neighbor vertex of the vertex.
-        """
         return self.neighbors.get(label)
 
     def remove_edge(self, edge: "Edge"):
-        """
-        Remove an edge from the vertex.
-        """
         if edge.label in self.__edges.keys():
             del self.__edges[edge.label]
         else:
             raise ValueError("This edge does not exist.")
 
     def get_weight(self, vertex: "Vertex") -> float:
-        """
-        Return the weight of the edge.
-        """
         return self.__edges.get(vertex.label).weight
 
     @property
     def edges(self) -> dict:
-        """
-        Return the edges of the vertex.
-        """
         return self.__edges
 
     def add_edge(self, edge: "Edge"):
-        """
-        Add an edge to the vertex.
-        """
         self.__edges[edge.label] = edge
         
     def edge(self, label: str) -> str:
-        """
-        Return the edge of the vertex.
-        """
         return self.edges.get(label)
 
     @property
     def loops(self) -> dict:
-        """
-        Return the loops of the vertex.
-        """
         return self.__loops
 
     def loop(self, label: str) -> str:
-        """
-        Return the loop of the vertex.
-        """
         return self.loops.get(label)
 
     def is_isolated(self) -> bool:
-        """
-        Return True if the vertex is isolated.
-        """
         return len(self.neighbors) == 0
 
 
@@ -120,98 +79,79 @@ class Edge:
     def __init__(
         self,
         label: str,
-        lvertex: Vertex,
-        rvertex: Vertex,
+        left_vertex: Vertex,
+        right_vertex: Vertex,
         weight: float = None,
         orientation: str = None,
     ):
         orientations = {
-            "lr": {"start": lvertex, "end": rvertex},
-            "rl": {"start": rvertex, "end": lvertex},
+            "->": {"start": left_vertex, "end": right_vertex},
+            "<-": {"start": right_vertex, "end": left_vertex},
         }
         self.__label = label
-        self.__lvertex = lvertex
-        self.__rvertex = rvertex
+        self.__left_vertex = left_vertex
+        self.__right_vertex = right_vertex
         self.__weight = weight
+        self.__orientation = self.validate_and_get_orientation(orientation)
         self.__start = (
             None
-            if orientation not in ["lr", "rl"]
+            if orientation not in orientations.keys()
             else orientations.get(orientation).get("start")
         )
         self.__end = (
             None
-            if orientation not in ["lr", "rl"]
+            if orientation not in orientations.keys()
             else orientations.get(orientation).get("end")
         )
-        self.__loop = True if lvertex == rvertex else False
+        self.__loop = True if left_vertex == right_vertex else False
 
-        lvertex.add_edge(self)
-        rvertex.add_edge(self)
+        left_vertex.add_edge(self)
+        right_vertex.add_edge(self)
+
+    def validate_and_get_orientation(self, orientation: str) -> bool:
+        if orientation in ["->", "<-", None]:
+            return orientation
+        else:
+            raise ValueError("The orientation is not valid.")
 
     def __del__(self):
-        """
-        Delete the edge.
-        """
         print(f"Edge {self.label} is deleted.")
 
     @property
     def label(self) -> str:
-        """
-        Return the label of the edge.
-        """
         return self.__label
 
     @property
-    def lvertex(self) -> Vertex:
-        """
-        Return the left vertex of the edge.
-        """
-        return self.__lvertex
+    def left_vertex(self) -> Vertex:
+        return self.__left_vertex
 
     @property
-    def rvertex(self) -> Vertex:
-        """
-        Return the right vertex of the edge.
-        """
-        return self.__rvertex
+    def right_vertex(self) -> Vertex:
+        return self.__right_vertex
 
     @property
     def weight(self) -> float:
-        """
-        Return the weight of the edge.
-        """
         return self.__weight
 
     @weight.setter
     def weight(self, weight: float):
-        """
-        Set the weight of the edge.
-        """
         self.__weight = weight
+    
+    @property
+    def orientation(self) -> str:
+        return self.__orientation
 
     @property
     def start(self) -> Vertex:
-        """
-        Return the start vertex of the edge.
-        """
         return self.__start
 
     @property
     def end(self) -> Vertex:
-        """
-        Return the end vertex of the edge.
-        """
         return self.__end
 
     @property
     def loop(self) -> bool:
-        """
-        Return True if the edge is a loop.
-        """
         return self.__loop
 
     def is_vertex(self, vertex_label: str) -> bool:
-        """
-        Return True if the vertex is the start or end vertex of the edge.
-        """
-        return self.lvertex.label == vertex_label or self.rvertex.label == vertex_label
+        return self.left_vertex.label == vertex_label or self.right_vertex.label == vertex_label
