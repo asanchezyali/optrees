7d7b6b6bd32db009bfb7ed4a695b1dcad1ef33c5
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2023-05-13 14:39:14 -0500

    add getMinimumSpanningTree function to __init__.py

--- optrees/__init__.py
+++ optrees/__init__.py
@@ -3,6 +3,8 @@
 from .graph.basic_objects import Edge, Vertex
 from .graph.graph import Graph
 from .optimal_trees_algorithms.boruvka import boruvka
+from .optimal_trees_algorithms.getMinimumSpanningTree import \
+    getMinimumSpanningTree
 from .optimal_trees_algorithms.kruskal import kruskal
 from .optimal_trees_algorithms.prim import prim
 from .version import __version__
@@ -12,6 +14,7 @@ __all__ = [
     "Edge",
     "Graph",
     "boruvka",
+    "getMinimumSpanningTree",
     "kruskal",
     "prim",
 ]
